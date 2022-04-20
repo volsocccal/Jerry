c990d3ff9f5e3de198aa86a52d8d9715915c2aac
fsaporito <sap93@gmail.com>
2022-04-20 12:54:02 +0200

    simplified main by refactoring readDF function

--- main.py
+++ main.py
@@ -2,56 +2,41 @@ import time
 import pandas as pd
 
 
-def readAnagrafiche() -> pd.Dataframe:
-    elenco_totale_path = "elenco_anagrafiche_qualifiche.xlsx"
-    print("Reading " + elenco_totale_path)
-    df_elenco_totale = pd.read_excel(elenco_totale_path, sheet_name="Report")
-    df_elenco_totale.drop(df_elenco_totale.tail(1).index,inplace=True) # Remove Last Row
+def readDf(INPUT_FOLDER: str, fileName: str):
+    path = INPUT_FOLDER + '//' + fileName
+    print("Reading " + path)
+    df = pd.read_excel(path, sheet_name="Report")
+    df.drop(df.tail(1).index,inplace=True) # Remove Last Row
     print(" ")
     time.sleep(0.3)
-    return df_elenco_totale
-
-def readServizi() -> pd.Dataframe:
-    elenco_servizi_path = "elenco_servizi.xlsx"
-    print("Reading " + elenco_servizi_path)
-    df_elenco_servizi = pd.read_excel(elenco_servizi_path, sheet_name="Report")
-    df_elenco_servizi.drop(df_elenco_servizi.tail(1).index,inplace=True) # Remove Last Row
-    df_elenco_servizi.drop(['GG', 'Partenza', 'Arrivo', 'Km inizio', 'Km fine', 'Km', 'Assistiti', 'N. Serv.', 'F. Viag.', 'Importo', 'Per.Fatturazione'], axis=1,inplace=True)
-    #df_elenco_servizi_personale = elaborate_servizi(df_elenco_servizi)
-    print(" ")
-    time.sleep(0.3)
-    return df_elenco_servizi
+    return df
 
 
 def main():
+
+    INPUT_FOLDER = "data//"
     
     # Log Start Time
     start_time = time.process_time()
 
     # Read Anagraphic DF
-    df_elenco_totale = readAnagrafiche()
+    df_elenco_totale = readDf(INPUT_FOLDER, "elenco_anagrafiche_qualifiche.xlsx")
+
+    # Read Formazione
+    df_formazione = readDf(INPUT_FOLDER, "elenco_formazione.xlsx")
 
     # Read Servizi
-    df_elenco_servizi = readServizi()
+    df_elenco_servizi = readDf(INPUT_FOLDER, "elenco_servizi.xlsx")
+    df_elenco_servizi.drop(['GG', 'Partenza', 'Arrivo', 'Km inizio', 'Km fine', 'Km', 'Assistiti', 'N. Serv.', 'F. Viag.', 'Importo', 'Per.Fatturazione'], axis=1,inplace=True)
 
     # Read TURNI
-    elenco_turni_path = "elenco_turni.xlsx"
-    print("Reading " + elenco_turni_path)
-    df_elenco_turni = pd.read_excel(elenco_turni_path, sheet_name="Report")
-    df_elenco_turni.drop(df_elenco_turni.tail(1).index,inplace=True) # Remove Last Row
-    print(" ")
-    time.sleep(0.3)
+    df_elenco_turni = readDf(INPUT_FOLDER, "elenco_turni.xlsx")
     
-     # Read PRESENZE
-    elenco_presenze_path = "elenco_presenze.xlsx"
-    print("Reading " + elenco_presenze_path)
-    df_presenze = pd.read_excel(elenco_presenze_path, sheet_name="Report")
-    df_presenze.drop(df_presenze.tail(1).index,inplace=True) # Remove Last Row
-    print(" ")
-    time.sleep(0.3)
+    # Read PRESENZE
+    df_presenze = readDf(INPUT_FOLDER, "elenco_presenze.xlsx")
 
     # Output Folder
-    OUTPUT_FOLDER = "../../Tommy Data/"
+    OUTPUT_FOLDER = "output//"
 
     # Log End Time
     end_time = time.process_time()
